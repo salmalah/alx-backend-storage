7b6bd2f17c3a33584ccd9dcfc0446bbb1d1a38b4
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-18 23:31:24 +0100

    Update 8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,7 +1,4 @@
 -- Task Description: Create an index idx_name_first on the table names and the first letter of name
 
 -- Create the index
-CREATE INDEX idx_name_first ON names (LEFT(name, 1));
-
--- Show indexes to confirm creation
-SHOW INDEX FROM names;
+CREATE INDEX idx_name_first ON names (name(1));
