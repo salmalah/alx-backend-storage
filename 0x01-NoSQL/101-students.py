85eba8629424a49e18745dc283debed794be09fe
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-22 21:58:05 +0100

    Create  101-students.py

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+this module is the define a Python function that returns all students sorted by average score
+"""
+
+
+def top_students(mongo_collection):
+  """
+  return list of students
+  """
+  pipeline = [
+        {
+            "$project": {
+                "name": "$name",
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+        {"$sort": {"averageScore": -1}}
+    ]
+    sorted_students = collection.aggregate(aggregation_pipeline)
+
+    return sorted_students
