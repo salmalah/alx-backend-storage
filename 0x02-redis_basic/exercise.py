f8d1cf08ec8c26f816035ba3dd7687a019730fdf
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-24 22:39:48 +0100

    Update exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -72,3 +72,17 @@ class Cache:
             Union[int, None]: The retrieved data as an integer.
         """
         return self.get(key, fn=int)
+    
+# Example usage in main.py
+if __name__ == "__main__":
+    cache = Cache()
+
+    TEST_CASES = {
+        b"foo": None,
+        123: int,
+        "bar": lambda d: d.decode("utf-8")
+    }
+
+    for value, fn in TEST_CASES.items():
+        key = cache.store(value)
+        assert cache.get(key, fn=fn) == value
