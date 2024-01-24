744db7468b39b7e8d743184f5c0cd1cfd2148408
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-24 23:01:51 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,40 +2,33 @@
 """
 This module is to defile Redis Module
 """
-
-import redis
-import requests
 from typing import Callable
 from functools import wraps
+import redis
+import requests
+redis_client = redis.Redis()
 
-redis = redis.Redis()
 
-
-def wrap_requests(fn: Callable) -> Callable:
-    """
-    wrapping decortors for requests
-    """
-
-    @wraps(fn)
-    def wrapper(url):
-        """
-        wrapping fucntors for requests
-        """
-        redis.incr(f"count:{url}")
-        cash_resp = redis.get(f"cached:{url}")
-        if cash_resp:
-            return cash_resp.decode('utf-8')
-        rslt = fn(url)
-        redis.setex(f"cached:{url}", 10, rslt)
-        return rslt
-
+def url_count(method: Callable) -> Callable:
+    """counts how many times an url is accessed"""
+    @wraps(method)
+    def wrapper(*args, **kwargs):
+        url = args[0]
+        redis_client.incr(f"count:{url}")
+        cached = redis_client.get(f'{url}')
+        if cached:
+            return cached.decode('utf-8')
+        redis_client.setex(f'{url}, 10, {method(url)}')
+        return method(*args, **kwargs)
     return wrapper
 
 
-@wrap_requests
+@url_count
 def get_page(url: str) -> str:
-    """
-    gets the page
-    """
-    resp = requests.get(url)
-    return resp.text
+    """get a page and cache value"""
+    response = requests.get(url)
+    return response.text
+
+
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
